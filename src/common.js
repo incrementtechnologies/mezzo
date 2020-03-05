f2c81fe22477f347818d5a218b3c8ff0d3372244
Kennette Canales <kennettecanales@gmail.com>
2020-03-05 19:03:50 +0800

    add body

--- src/common.js
+++ src/common.js
@@ -1,5 +1,8 @@
 export default{
   APP_NAME: 'Mezzo Hotel',
   APP_EMAIL: 'admin@mezzohotel.com',
-  APP_COPYRIGHT: '@Mezzo Hotel ' + new Date().getFullYear()
+  APP_COPYRIGHT: '@Mezzo Hotel ' + new Date().getFullYear(),
+  socialMedia: {
+    facebook: 'https://www.facebook.com/mezzohotelcebu'
+  }
 }
---
+++ src/components/frame/body.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="body">
+    <div class="jumbotron text-center">
+      <h1 class="display-4">Hello World!</h1>
+      <p>Welcome to Mezzo Hotel</p>
+    </div>
+  </div>
+</template>
+<style scoped>
+  .body{
+    height: 100vh;
+  }
+</style>
