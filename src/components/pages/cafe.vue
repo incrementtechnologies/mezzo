c2f0fa71c6b7c8d594ac9a255746338719fefcf2
Kennette Canales <kennettecanales@gmail.com>
2020-09-04 01:16:37 +0800

    update

---
+++ src/components/pages/cafe.vue
@@ -0,0 +1,227 @@
+<p>
+<style>
+.menu-images{
+  list-style: none;
+  width: 100%;
+  float: left;
+  margin-bottom: 25px;
+}
+
+.menu-images li{
+  width: 48%;
+  height: auto;
+  float: left;
+  border-bottom: solid 1px #003;
+  margin-right: 2%;
+}
+
+.menu-images li label{
+  margin: 0px;
+  width: 100%;
+  float: left;
+  color: #003;
+  height: 50px;
+}
+
+.menu-images li .description{
+  margin: 0px;
+  width: 100%;
+  float: left;
+  color: #555;
+  height: 50px;
+}
+
+.menu-images li label .title{
+  padding-top: 10px;
+  padding-bottom: 10px;
+  width: 70%;
+  float: left;
+}
+
+.menu-images li label .price{
+  padding-top: 10px;
+  padding-bottom: 10px;
+  width: 30%;
+  float: left;
+  color: #FF0000;
+}
+
+.btn-text{
+  color: white;
+  width: 100%;
+  text-align: center;
+}
+
+.btn-text:hover{
+  cursor: pointer;
+  color: white;
+}
+
+.custom-button{
+  width: 20% !important;
+}
+
+h5{
+  color: #FF0000;
+}
+
+@media (max-width: 991px){
+  .menu-images li{
+    width: 100%;
+    height: auto;
+    float: left;
+  }
+
+  .custom-button{
+    width: 60% !important;
+  }
+
+  .menu-images li .description{
+    margin: 0px;
+    width: 100%;
+    float: left;
+    color: #555;
+    height: auto;
+  }
+}
+
+</style>
+<h5>APPETIZER</h5>
+<ul class="menu-images">
+  <li>
+    <label>
+      <b class="title">CHILI TOFU</b>
+      <b class="text-right price">PHP 178</b>
+    </label>
+    <label class="description">
+      Curdled white soy bean milk coated with herbed crumb served with homemade chili sauce
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">INDIAN SPICE ROTI</b>
+      <b class="text-right price">PHP 178</b>
+    </label>
+    <label class="description">
+      Stoned ground wholemeal flat bread served with curry sauce
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">SALTED EGG POTATO</b>
+      <b class="text-right price">PHP 218</b>
+    </label>
+    <label class="description">
+      Deep fried salted egg potato chips garnished with curry leaf and chilli flakes
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">SHRIMP LOLLIPOP</b>
+      <b class="text-right price">PHP 238</b>
+    </label>
+    <label class="description">
+      Spicy shrimp puree moulded around lemo grass served with Almond Pesto
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">PLUM CHICKEN WINGS</b>
+      <b class="text-right price">PHP 258</b>
+    </label>
+    <label class="description">
+      Seasoned with our special mix of spices. Baked until golden brown. Tossed in hot and tangy barbecue sauce
+    </label>
+  </li>
+</ul>
+
+<h5>Soup</h5>
+<ul class="menu-images">
+  <li>
+    <label>
+      <b class="title">SEAFOOD CHOWDER</b>
+      <b class="text-right price">PHP 128</b>
+    </label>
+    <label class="description">
+      Hearty seafood treasure with chunks of vegetables and fish seasoned with local herbs
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">THAI HOT AND SOUR SOUP</b>
+      <b class="text-right price">PHP 178</b>
+    </label>
+    <label class="description">
+      Savoury spicy shrimps with Kaffir Lime Leaf
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">SINIGANG NA BABOY</b>
+      <b class="text-right price">PHP 248</b>
+    </label>
+    <label class="description">
+      A tamarind-infused Filipino staple
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">SINIGANG NA HIPON</b>
+      <b class="text-right price">PHP 348</b>
+    </label>
+    <label class="description">
+      Tamarind flavoured shrimp with island vegetables
+    </label>
+  </li>
+</ul>
+
+<h5>Salad</h5>
+<ul class="menu-images">
+  <li>
+    <label>
+      <b class="title">FATTOUSH SALAD</b>
+      <b class="text-right price">PHP 168</b>
+    </label>
+    <label class="description">
+      Mediterranean salad with toasted flat bread combined with mixed greens in yoghurt dressing
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">MESCLUN SALAD</b>
+      <b class="text-right price">PHP 198</b>
+    </label>
+    <label class="description">
+      An array of seasonal garden greens with your choice of vinaigrette or thousand island dressing
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">CHICKEN POMELO SALAD</b>
+      <b class="text-right price">PHP 258</b>
+    </label>
+    <label class="description">
+      Combination of chicken and pomelo tossed in zesty homemade Cambodian dressing
+    </label>
+  </li>
+
+  <li>
+    <label>
+      <b class="title">SHRIMP CAESAR</b>
+      <b class="text-right price">PHP 258</b>
+    </label>
+    <label class="description">
+      Japanesse-inspired tempura served with assortment of lettuce in Caesar salad dressing
+    </label>
+  </li>
+</ul>
+</p>
