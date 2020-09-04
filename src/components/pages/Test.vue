82662e59bba5cd352535bb7dcdbab42ffc2f5cfb
kennCK <kennettecanales@gmail.com>
2020-09-04 23:25:09 +0800

    update

---
+++ src/components/pages/Test.vue
@@ -0,0 +1,57 @@
+<p>
+<style>
+.menu-images{
+  list-style: none;
+  width: 100%;
+  float: left;
+  margin-bottom: 25px;
+}
+
+.menu-images li{
+  width: 50%;
+  height: auto;
+  float: left;
+}
+
+.menu-images li img{
+  width: 100%;
+  height: auto;
+  float: left;
+}
+
+@media (max-width: 991px){
+  .menu-images li{
+    width: 100%;
+    height: auto;
+    float: left;
+  }
+
+  .custom-button{
+    width: 60% !important;
+  }
+}
+
+</style>
+<p>
+Rising from the long months of COVID,  your safety and wellbeing is our topmost priority.
+</p>
+<p>
+As we move forward to a new normal, we are making your stay safer by observing these protocols as you go in and out of the hotel's premises, thoroughly disinfecting and sanitizing every room and corners, and strict observation of social distancing. We have also added options for serving meals as we would highly recommend having no direct contact with guests and the hotel's associates for safety measures.
+</p>
+<ul class="menu-images">
+  <li>
+    <img src="https://mezzohotel.com/img/precaution1.png" />
+  </li>
+  <li>
+    <img src="https://mezzohotel.com/img/precaution2.png" />
+  </li>
+  <li>
+    <img src="https://mezzohotel.com/img/precaution3.png" />
+  </li>
+  <li>
+    <img src="https://mezzohotel.com/img/precaution4.png" />
+  </li>
+</ul>
+<p>
+As we hope for better days and as we continue to serve and provide you a safe place. Please be reminded too that we will require a health declaration form, you may be able to scan. QR Code is available at our lobby and front desk and proper health protocols are strictly followed.
+</p>
