5d60bdfae684a3ee72140aae8784e8977174a04e
kennck <kennettecanales@gmail.com>
2020-09-04 15:15:54 +0800

    update

--- src/components/pages/cafe.vue
+++ src/components/pages/cafe.vue
@@ -1,227 +1,608 @@
 <p>
 <style>
 .menu-images{
-  list-style: none;
-  width: 100%;
-  float: left;
-  margin-bottom: 25px;
+ list-style: none;
+ width: 100%;
+ float: left;
+ margin-bottom: 25px;
 }
 
 .menu-images li{
-  width: 48%;
-  height: auto;
-  float: left;
-  border-bottom: solid 1px #003;
-  margin-right: 2%;
+ width: 48%;
+ height: auto;
+ float: left;
+ border-bottom: solid 1px #003;
+ margin-right: 2%;
 }
 
 .menu-images li label{
-  margin: 0px;
-  width: 100%;
-  float: left;
-  color: #003;
-  height: 50px;
+ margin: 0px;
+ width: 100%;
+ float: left;
+ color: #003;
+ height: 50px;
 }
 
 .menu-images li .description{
-  margin: 0px;
-  width: 100%;
-  float: left;
-  color: #555;
-  height: 50px;
+ margin: 0px;
+ width: 100%;
+ float: left;
+ color: #555;
+ height: 50px;
 }
 
 .menu-images li label .title{
-  padding-top: 10px;
-  padding-bottom: 10px;
-  width: 70%;
-  float: left;
+ padding-top: 10px;
+ padding-bottom: 10px;
+ width: 70%;
+ float: left;
 }
 
 .menu-images li label .price{
-  padding-top: 10px;
-  padding-bottom: 10px;
-  width: 30%;
-  float: left;
-  color: #FF0000;
+ padding-top: 10px;
+ padding-bottom: 10px;
+ width: 30%;
+ float: left;
+ color: #cbab58;
 }
 
 .btn-text{
-  color: white;
-  width: 100%;
-  text-align: center;
+ color: white;
+ width: 100%;
+ text-align: center;
 }
 
 .btn-text:hover{
-  cursor: pointer;
-  color: white;
+ cursor: pointer;
+ color: white;
 }
 
 .custom-button{
-  width: 20% !important;
+ width: 20% !important;
 }
 
 h5{
-  color: #FF0000;
+ color: #cbab58;
 }
 
 @media (max-width: 991px){
-  .menu-images li{
-    width: 100%;
-    height: auto;
-    float: left;
-  }
+ .menu-images li{
+ width: 100%;
+ height: auto;
+ float: left;
+ }
 
-  .custom-button{
-    width: 60% !important;
-  }
+ .custom-button{
+ width: 60% !important;
+ }
 
-  .menu-images li .description{
-    margin: 0px;
-    width: 100%;
-    float: left;
-    color: #555;
-    height: auto;
-  }
+ .menu-images li .description{
+ margin: 0px;
+ width: 100%;
+ float: left;
+ color: #555;
+ height: auto;
+ }
 }
 
 </style>
 <h5>APPETIZER</h5>
 <ul class="menu-images">
-  <li>
-    <label>
-      <b class="title">CHILI TOFU</b>
-      <b class="text-right price">PHP 178</b>
-    </label>
-    <label class="description">
-      Curdled white soy bean milk coated with herbed crumb served with homemade chili sauce
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">CHILI TOFU</b>
+ <b class="text-right price">PHP 178</b>
+ </label>
+ <label class="description">
+ Curdled white soy bean milk coated with herbed crumb served with homemade sweet chili sauce
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">INDIAN SPICE ROTI</b>
-      <b class="text-right price">PHP 178</b>
-    </label>
-    <label class="description">
-      Stoned ground wholemeal flat bread served with curry sauce
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">INDIAN SPICE ROTI</b>
+ <b class="text-right price">PHP 178</b>
+ </label>
+ <label class="description">
+ Stoned ground wholemeal flat bread served with curry sauce
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">SALTED EGG POTATO</b>
-      <b class="text-right price">PHP 218</b>
-    </label>
-    <label class="description">
-      Deep fried salted egg potato chips garnished with curry leaf and chilli flakes
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">SALTED EGG POTATO</b>
+ <b class="text-right price">PHP 218</b>
+ </label>
+ <label class="description">
+ Deep fried salted egg potato chips garnished with curry leaf and chilli flakes
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">SHRIMP LOLLIPOP</b>
-      <b class="text-right price">PHP 238</b>
-    </label>
-    <label class="description">
-      Spicy shrimp puree moulded around lemo grass served with Almond Pesto
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">SHRIMP LOLLIPOP</b>
+ <b class="text-right price">PHP 238</b>
+ </label>
+ <label class="description">
+ Spicy shrimp purée moulded around lemon grass served with Almond Pesto
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">PLUM CHICKEN WINGS</b>
-      <b class="text-right price">PHP 258</b>
-    </label>
-    <label class="description">
-      Seasoned with our special mix of spices. Baked until golden brown. Tossed in hot and tangy barbecue sauce
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">PLUM CHICKEN WINGS</b>
+ <b class="text-right price">PHP 258</b>
+ </label>
+ <label class="description">
+ Seasoned with our special mix of spices. Baked until golden brown. Tossed in hot and tangy barbecue sauce
+ </label>
+ </li>
 </ul>
 
-<h5>Soup</h5>
+<h5>SOUP</h5>
 <ul class="menu-images">
-  <li>
-    <label>
-      <b class="title">SEAFOOD CHOWDER</b>
-      <b class="text-right price">PHP 128</b>
-    </label>
-    <label class="description">
-      Hearty seafood treasure with chunks of vegetables and fish seasoned with local herbs
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">SEAFOOD CHOWDER</b>
+ <b class="text-right price">PHP 128</b>
+ </label>
+ <label class="description">
+ Hearty seafood treasure with chunks of vegetables and fish seasoned with local herbs
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">THAI HOT AND SOUR SOUP</b>
-      <b class="text-right price">PHP 178</b>
-    </label>
-    <label class="description">
-      Savoury spicy shrimps with Kaffir Lime Leaf
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">THAI HOT AND SOUR SOUP</b>
+ <b class="text-right price">PHP 178</b>
+ </label>
+ <label class="description">
+ Savoury spicy shrimp with Kaffir Lime Leaf
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">SINIGANG NA BABOY</b>
-      <b class="text-right price">PHP 248</b>
-    </label>
-    <label class="description">
-      A tamarind-infused Filipino staple
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">SINIGANG NA BABOY</b>
+ <b class="text-right price">PHP 248</b>
+ </label>
+ <label class="description">
+ A tamarind-infused Filipino staple
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">SINIGANG NA HIPON</b>
-      <b class="text-right price">PHP 348</b>
-    </label>
-    <label class="description">
-      Tamarind flavoured shrimp with island vegetables
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">SINIGANG NA HIPON</b>
+ <b class="text-right price">PHP 348</b>
+ </label>
+ <label class="description">
+ Tamarind flavoured shrimp with island vegetables
+ </label>
+ </li>
 </ul>
 
-<h5>Salad</h5>
+<h5>SALAD</h5>
 <ul class="menu-images">
-  <li>
-    <label>
-      <b class="title">FATTOUSH SALAD</b>
-      <b class="text-right price">PHP 168</b>
-    </label>
-    <label class="description">
-      Mediterranean salad with toasted flat bread combined with mixed greens in yoghurt dressing
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">FATTOUSH SALAD</b>
+ <b class="text-right price">PHP 168</b>
+ </label>
+ <label class="description">
+ Mediterranean salad with toasted flat bread combined with mixed greens in yoghurt dressing
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">MESCLUN SALAD</b>
-      <b class="text-right price">PHP 198</b>
-    </label>
-    <label class="description">
-      An array of seasonal garden greens with your choice of vinaigrette or thousand island dressing
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">MESCLUN SALAD</b>
+ <b class="text-right price">PHP 198</b>
+ </label>
+ <label class="description">
+ An array of seasonal garden greens with your choice of vinaigrette or thousand island dressing
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">CHICKEN POMELO SALAD</b>
-      <b class="text-right price">PHP 258</b>
-    </label>
-    <label class="description">
-      Combination of chicken and pomelo tossed in zesty homemade Cambodian dressing
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">CHICKEN POMELO SALAD</b>
+ <b class="text-right price">PHP 258</b>
+ </label>
+ <label class="description">
+ Combination of chicken and pomelo tossed in zesty homemade Cambodian dressing
+ </label>
+ </li>
 
-  <li>
-    <label>
-      <b class="title">SHRIMP CAESAR</b>
-      <b class="text-right price">PHP 258</b>
-    </label>
-    <label class="description">
-      Japanesse-inspired tempura served with assortment of lettuce in Caesar salad dressing
-    </label>
-  </li>
+ <li>
+ <label>
+ <b class="title">SHRIMP CAESAR</b>
+ <b class="text-right price">PHP 258</b>
+ </label>
+ <label class="description">
+ Japanese-inspired tempura served with assortment of lettuce in Caesar salad dressing
+ </label>
+ </li>
+</ul>
+
+<h5>MAIN COURSE</h5>
+<ul class="menu-images">
+ <li>
+ <label>
+ <b class="title">PORK SKORDALIA</b>
+ <b class="text-right price">PHP 228</b>
+ </label>
+ <label class="description">
+ Crispy pork belly on a bed of local mashed root vegetable with spicy
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">AMARA CHICKEN</b>
+ <b class="text-right price">PHP 248</b>
+ </label>
+ <label class="description">
+ East meets West grilled chicken legs served with fruit salsa and beurre blanc
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">HOISIN CHICKEN</b>
+ <b class="text-right price">PHP 248</b>
+ </label>
+ <label class="description">
+ Fragrant marinated chicken quarters served with Asian Napa slaw
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">WOK-SEARED BABY PRAWNS</b>
+ <b class="text-right price">PHP 258</b>
+ </label>
+ <label class="description">
+ Baby prawns infused with herb butter sauce
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">DONGPO PORK</b>
+ <b class="text-right price">PHP 258</b>
+ </label>
+ <label class="description">
+ Braised succulent Chinese-style pork served with bok choy
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">MEZZO CHICKEN</b>
+ <b class="text-right price">PHP 268</b>
+ </label>
+ <label class="description">
+ Panko breaded chicken roulade stuffed with creamy spinach served with vegetable of the day
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">PAN FRIED FISH WITH SPINACH AND HERBS</b>
+ <b class="text-right price">PHP 278</b>
+ </label>
+ <label class="description">
+ Seared rock fish on a bed of spinach with light-herbed cream sauce
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">GRILLED FISH WITH CREOLE SAUCE</b>
+ <b class="text-right price">PHP 278</b>
+ </label>
+ <label class="description">
+ Broiled Cebuana fish with a seychellois twist served with spicy creole sauce
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">WEINER SCHNITZEL</b>
+ <b class="text-right price">PHP 298</b>
+ </label>
+ <label class="description">
+ Medallion of pork dredged in herbed crumbs with cafe de Paris buffer
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">GINGER AND SCALLION BEEF</b>
+ <b class="text-right price">PHP 328</b>
+ </label>
+ <label class="description">
+ Shredded Tender fillet of beef with caramelized ginger
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">CUBAN RIBS</b>
+ <b class="text-right price">PHP 338</b>
+ </label>
+ <label class="description">
+ Fragrant savoury roasted pork served with mashed potato
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">BEEF TOURNEDOS</b>
+ <b class="text-right price">PHP 518</b>
+ </label>
+ <label class="description">
+ Grilled beef tenderloin glazed with red wine served with assorted vegetable of the day
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">HERB CRUMBED CHICKEN</b>
+ <b class="text-right price">Php 248.00</b>
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">BABY BACK RIBS</b>
+ <b class="text-right price">Php 438.00</b>
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">SHRIMP TEMPURA</b>
+ <b class="text-right price">Php 398.00</b>
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">FISH AND CHIPS</b>
+ <b class="text-right price">Php 348.00</b>
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">BRAISED BEEF ROULADE</b>
+ <b class="text-right price">Php 428.00</b>
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">CHEESE TONKATSU</b>
+ <b class="text-right price">Php 348.00</b>
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">CHICKEN TERIYAKI</b>
+ <b class="text-right price">Php 298.00</b>
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">MEZZO FESTIVAL</b>
+ <b class="text-right price">Php 788.00</b>
+ </label>
+ <label class="description">
+ Good for sharing. Perfec for 4-5 persons
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">Oriental</b>
+ </label>
+ <label class="description">
+ Shish Tawook, Falafel, Hummus, Crispy Fried Fish Fillet, Green Salads, Assorted Olives, Tortilla Breads.
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">Malay</b>
+ </label>
+ <label class="description">
+ Assorted Satay, Nasi Goreng with Fried Egg, Sambal Balado, Green Salad with freshly grated coconut, corn on the cob.
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">Indies</b>
+ </label>
+ <label class="description">
+ Spiced Ribs, Grilled Pineapple, Roasted Potatoes, Grilled Prawns with spicy tomato sauce, green salad, whole wheat rolls, fresh crudites.
+ </label>
+ </li>
+</ul>
+
+<h5>PIZZA</h5>
+<ul class="menu-images">
+ <li>
+ <label>
+ <b class="title">HAWAIIAN</b>
+ <b class="text-right price">PHP 248</b>
+ </label>
+ <label class="description">
+ A classic combination of ham, pineapple and homemade tomato sauce
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">MARGHERITA</b>
+ <b class="text-right price">PHP 258</b>
+ </label>
+ <label class="description">
+ A typical neopolitan pizza with tomato sauce annd slices
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">BARBEQUE</b>
+ <b class="text-right price">PHP 298</b>
+ </label>
+ <label class="description">
+ South American house smoked pork barbeque topped with onion and mozzarella
+ </label>
+ </li>
+</ul>
+
+<h5>VEGETABLE</h5>
+<ul class="menu-images">
+ <li>
+ <label>
+ <b class="title">EGGPLANT SATAY</b>
+ <b class="text-right price">PHP 168</b>
+ </label>
+ <label class="description">
+ Batonnet Aubergine served with homemade peanut sauce
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">GRILLED VEGETABLES</b>
+ <b class="text-right price">PHP 178</b>
+ </label>
+ <label class="description">
+ Assorted fresh garden vegetables grilled in olive oil and Italian herbs
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">STIR-FRIED SHRIMP IN CHAYOTE</b>
+ <b class="text-right price">PHP 178</b>
+ </label>
+ <label class="description">
+ Chinese wine deglazed chayote dusted in Asian spice
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">VEGETABLE MORNAY</b>
+ <b class="text-right price">PHP 208</b>
+ </label>
+ <label class="description">
+ Baked assortment of vegetables in cheese sauce
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">VEGETABLES STIR FRY IN OYSTER SAUCE</b>
+ <b class="text-right price">PHP 228</b>
+ </label>
+ <label class="description">
+ Seasonal vegetable sautéed in Chinese wine with oyster sauce
+ </label>
+ </li>
+</ul>
+
+<h5>PASTA & NOODLE</h5>
+<ul class="menu-images">
+ <li>
+ <label>
+ <b class="title">SEAFOOD PASTA</b>
+ <b class="text-right price">PHP 248</b>
+ </label>
+ <label class="description">
+ Filling concoction of pasta and seafood
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">PAD THAI</b>
+ <b class="text-right price">PHP 248</b>
+ </label>
+ <label class="description">
+ Thai style stir-fried noodles with vegetable and shrimp
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">SPAGHETTI BOLOGNESE</b>
+ <b class="text-right price">PHP 278</b>
+ </label>
+ <label class="description">
+ Classic bolognese sauce flamed with red wine
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">SHRIMP CARBONARA</b>
+ <b class="text-right price">PHP 288</b>
+ </label>
+ <label class="description">
+ Al dente noodle with creamy shrimp, bacon and egg
+ </label>
+ </li>
+</ul>
+
+<h5>GOURMET BITE</h5>
+<ul class="menu-images">
+ <li>
+ <label>
+ <b class="title">TUNA MELT</b>
+ <b class="text-right price">PHP 198</b>
+ </label>
+ <label class="description">
+ Ciabatta bread with tuna, salad and melted cheese
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">MEZZO CLUBHOUSE</b>
+ <b class="text-right price">PHP 208</b>
+ </label>
+ <label class="description">
+ Layered sandwich with ham, cheese and egg
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">MONGOLIAN BEEF ROLL</b>
+ <b class="text-right price">PHP 238</b>
+ </label>
+ <label class="description">
+ Beef tenderloin with traditional Asian marinade rolled with homemade tomato sauce
+ </label>
+ </li>
+
+ <li>
+ <label>
+ <b class="title">CHEESE BURGER WITH BARBBEQUE SAUCE</b>
+ <b class="text-right price">PHP 258</b>
+ </label>
+ <label class="description">
+ Californian beef patty topped with cheese and barbeque sauce
+ </label>
+ </li>
 </ul>
 </p>
