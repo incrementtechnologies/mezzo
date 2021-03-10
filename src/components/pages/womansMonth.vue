3eb0a8a2957e634dc01fb36109a712309d82f317
kennck <kennettecanales@gmail.com>
2021-03-10 14:31:25 +0800

    udpate

--- src/components/pages/womansMonth.vue
+++ src/components/pages/womansMonth.vue
@@ -31,52 +31,43 @@
 }
 
 </style>
-<template>
-  <div>
-    <p>
-        As we honor the brave ladies this Women's Month, we at Mezzo Hotel celebrate the strength and significance of <b>women</b> that highligths her extraordinary roles as changemakers in our society. 
-    </p>
-    <p>
-      <b>March like a woman with our lowest room rates </b>that starts at Php 2,000.00 a night with breakfast for two (2) and an exclusive 20% OFF in our Cafe Mezzo. Treat the woman who has inspired you with a perfect <b>staycation in the heart of Cebu City</b>
-    </p>
-    <ul class="menu-images">
+<p>
+    As we honor the brave ladies this Women's Month, we at Mezzo Hotel celebrate the strength and significance of <b>women</b> that highligths her extraordinary roles as changemakers in our society. 
+</p>
+<p>
+  <b>March like a woman with our lowest room rates </b>that starts at Php 2,000.00 a night with breakfast for two (2) and an exclusive 20% OFF in our Cafe Mezzo. Treat the woman who has inspired you with a perfect <b>staycation in the heart of Cebu City</b>
+</p>
+<p>
+We're giving <b>free refreshing drinks</b> to all the brave women with our Dip and Dine.
+</p>
+<ul class="menu-images">
   <li>
-    <img src="http://www.mezzohotel.com/img/women_month1_1.jpg" />
+    <img src="http://www.mezzohotel.com/img/women_month1_0.jpg" />
   </li>
-    </ul>
-    <p>
-    We're giving <b>free refreshing drinks</b> to all the brave women with our Dip and Dine.
-    </p>
-    <ul class="menu-images">
-      <li>
-        <img src="http://www.mezzohotel.com/img/women_month1_0.jpg" />
-      </li>
-    </ul>
-    <p>
-      We also want to celebrate women who have always been strong by making their big day more memorable one.
-      Capture your precious moments with Photo Booth and an overnight stay in our Deluxe Room for two all on us.
-    </p>
-    We are sponsoring your dream wedding with:
-    <dl>
-    <dd>-An overnight stay in Deluxe Room with breakfast for two</dd>
-    <dd>-Photo Booth for 2 hours during the event</dd>
-    <dd>-LCD Projector with screen for 4 hours</dd>
-    <dd>-Complimentary 1 hour extension of function room usage</dd>
-    <dd>-Waived handling fee for dessert buffet</dd>
-    <dd>-Waived bar fine for 4 bottles of wine</dd>
-    </dl>Mechanics:
-    <dl>
-    <dd>-This offer is available for social events held within March to December 2021.</dd>
-    <dd>-Confirmation of booking only on the basis of contract signed by client with promo duration March 1-31, 2021.</dd>
-    </dl>
-    <ul class="menu-images">
-      <li>
-        <img src="https://mezzohotel.com/img/women_month1_3.jpg" />
-      </li>
-    </ul>
-    <p>We are also recognizing our dedicated women associates as we highlight their stories with our social media campaign of <b>#MakingHerStory #WomenofMezzo</b>.</p>
-    <p>This is to show commitment to choose to challenge inequality, call out bias, question stereotypes, and help forge an inclusive world.</p>
-   <p> Here in Mezzo Hotel, we are celebrating Women's Month as a constant reminder of their significant contributions.
-  </p>
-  </div>
-</template>
+</ul>
+<p>
+  We also want to celebrate women who have always been strong by making their big day more memorable one.
+  Capture your precious moments with Photo Booth and an overnight stay in our Deluxe Room for two all on us.
+</p>
+We are sponsoring your dream wedding with:
+<dl>
+<dd>-An overnight stay in Deluxe Room with breakfast for two</dd>
+<dd>-Photo Booth for 2 hours during the event</dd>
+<dd>-LCD Projector with screen for 4 hours</dd>
+<dd>-Complimentary 1 hour extension of function room usage</dd>
+<dd>-Waived handling fee for dessert buffet</dd>
+<dd>-Waived bar fine for 4 bottles of wine</dd>
+</dl>Mechanics:
+<dl>
+<dd>-This offer is available for social events held within March to December 2021.</dd>
+<dd>-Confirmation of booking only on the basis of contract signed by client with promo duration March 1-31, 2021.</dd>
+</dl>
+<ul class="menu-images">
+  <li>
+    <img src="https://mezzohotel.com/img/women_month1_3.jpg" />
+  </li>
+</ul>
+<p>We are also recognizing our dedicated women associates as we highlight their stories with our social media campaign of <b>#MakingHerStory #WomenofMezzo</b>.</p>
+<p>This is to show commitment to choose to challenge inequality, call out bias, question stereotypes, and help forge an inclusive world.</p>
+<p> Here in Mezzo Hotel, we are celebrating Women's Month as a constant reminder of their significant contributions.
+</p>
