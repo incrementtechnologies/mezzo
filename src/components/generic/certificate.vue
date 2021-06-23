fc2bce319a74075c77404fb84fe661a6a9393406
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-06-23 16:03:30 +0800

    add award images

--- src/components/generic/certificate.vue
+++ src/components/generic/certificate.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <div class="image-holder">
-      <img :src="common.certificate">
+      <img :src="require('assets/img/recognition-excellence-2019.png')">
+      <img :src="require('assets/img/sluurpy-2020.png')">
+      <img :src="require('assets/img/tuvrheinland.png')">
+      <img :src="require('assets/img/travellers-choice-2021.png')">
+      <img :src="require('assets/img/safety-seal-covid.png')">
     </div>
   </div>
 </template>
 <style scoped lang="scss">
 @import "~assets/style/colors.scss";
 img{
-  width: 20%;
   border-radius: 10px;
-  margin-top: 50px;
+  width: 20%;
+  padding: 2%;
 }
 .image-holder{
   width: 100%;
@@ -18,8 +22,9 @@ img{
 }
 @media only screen and (max-width: 990px) {
   img {
-    width: 96%;
-    margin-left: 2%;
+    width: 60%;
+    margin-left: 20%;
+    margin-right: 20%;
     margin-top: 50px;
   }
 }
