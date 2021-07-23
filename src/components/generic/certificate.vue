a7ef5a288a70eacab3189c744fcf61771b4a6664
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-07-23 11:36:30 +0800

    Update certificates

--- src/components/generic/certificate.vue
+++ src/components/generic/certificate.vue
@@ -4,6 +4,7 @@
       <a href="https://m.certipedia.com/?id=9000010741" target="_blank"><img id="rec" :src="require('assets/img/tuvrheinland.png')"></a>
       <img :src="require('assets/img/recognition-excellence-2019.png')">
       <img :src="require('assets/img/sluurpy-2020.png')">
+      <img :src="require('assets/img/agoda.png')">
       <img :src="require('assets/img/travellers-choice-2021.png')">
       <img :src="require('assets/img/safety-seal-covid.png')">
       <img :src="require('assets/img/safe-travels.png')">
@@ -25,39 +26,69 @@ img{
   float: left;
   text-align: center;
 }
-@media only screen and (max-width: 1487px) {
+@media only screen and (max-width: 1725px) {
   img {
+    height: 130px;
+  }
+}
+
+@media only screen and (max-width: 1601px) {
+   img {
     height: 120px;
   }
 }
 
-@media only screen and (max-width: 1274px) {
+@media only screen and (max-width: 1478px) {
+   img {
+    height: 110px;
+  }
+}
+
+@media only screen and (max-width: 1355px) {
    img {
     height: 100px;
   }
 }
 
-@media only screen and (max-width: 1062px) {
-   img {
-    height: 95px;
-  }
-}
-
-@media only screen and (max-width: 1009px) {
+@media only screen and (max-width: 1232px) {
    img {
     height: 90px;
   }
 }
 
-@media only screen and (max-width: 956px) {
+@media only screen and (max-width: 1109px) {
    img {
-    height: 85px;
+    height: 80px;
   }
 }
 
-@media only screen and (max-width: 850px) {
+@media only screen and (max-width: 986px) {
+   img {
+    height: 110px;
+  }
+}
+
+@media only screen and (max-width: 692px) {
+   img {
+    height: 90px;
+  }
+}
+
+@media only screen and (max-width: 566px) {
+   img {
+    height: 115px;
+  }
+}
+
+@media only screen and (max-width: 459px) {
    img {
     height: 100px;
+  }
+}
+
+@media only screen and (max-width: 350px) {
+   img {
+    height: 90px;
   }
 }
 </style>
