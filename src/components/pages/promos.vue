a7e68ba2d2efe45a4ef06684b3ac3d5f657b0744
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2022-03-25 14:42:11 +0800

    update new promo

--- src/components/pages/promos.vue
+++ src/components/pages/promos.vue
@@ -1,41 +1,38 @@
 <template>
-  <div>
+  <div v-if="content !== null && content !== undefined">
     <span class="page-holder"> <Logo /></span>
     <span>
       <div class="blog-tiles">
         <button class="btn btn-warning" @click="redirect('/')">Home</button>
         <span class="blog-item">
-          <span class="details">
-            <h3 style="margin-top: 25px">PROMOS</h3>
-            Published by <b>Mezzo Hotel</b> on November 17,2021
-            <br />
-            <label>
-              We hope the Christmas season brings you joy and hope, and the
-              chance to make beautiful memories with loved ones.<br /><br />Here
-              are some ideas to help bring in the holiday cheer:
-            </label>
-            <p><img :src="'https://mezzohotel.com/img/christmas.png'" /></p>
-            <p><img :src="'https://mezzohotel.com/img/cmas_2.png'" /></p>
-            <p><img :src="'https://mezzohotel.com/img/cmas_3.png'" /></p>
-            <label
-              >Whether you’re looking to have a grand feast, an annual catch-up,
-              or a simple staycation, you can always
-              <b style="color: #000033">#MakeitMezzo</b>.<br /><br />Experience
-              a safe, relaxing, and luxurious holiday only at Mezzo Hotel.<br /><br />For
-              reservations and enquiries, call:
-              <a href="tel:(032) 231-0777">(032) 231-0777</a> or email us at
-              <a href="mailto:info@mezzohotel.com">info@mezzohotel.com</a
-              >.<br /><br /><b>Merry Christmas!</b></label
-            >
-          </span>
+          <label v-html="content"></label>
         </span>
       </div>
     </span>
     <MyFooter :property="'redirect'"></MyFooter>
   </div>
+  <span class="loading text-center" v-else-if="loading === true">
+    <font-awesome-icon
+      :icon="faCircleNotch"
+      class="fa-spin"
+    ></font-awesome-icon>
+  </span>
 </template>
 <style scoped lang="scss">
 @import "~assets/style/colors.scss";
+.fa-spin {
+  animation-duration: 0.5s;
+  margin-top: 40vh;
+}
+
+.loading {
+  font-size: 75px;
+  width: 100%;
+  color: $warning;
+  text-align: center;
+  height: 100vh;
+  float: left;
+}
 
 .page-holder {
   width: 100%;
@@ -49,12 +46,6 @@ button {
   margin-top: 20px;
 }
 
-img {
-  height: 700px !important;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  width: 700px !important;
-}
 .blog-tiles {
   width: 70%;
   float: left;
@@ -65,10 +56,6 @@ img {
   margin-top: 0px;
 }
 
-p {
-  text-align: center;
-}
-
 .blog-item {
   width: 100%;
   float: left;
@@ -77,38 +64,11 @@ p {
   margin-bottom: 25px;
 }
 
-.blog-item .details {
-  padding-left: 25px;
-  padding-bottom: 50px;
-  padding-right: 25px;
-  float: left;
-  width: 100%;
-  min-height: 50px;
-  text-align: justify;
-}
-
-.details label {
-  width: 100%;
-  margin-top: 25px;
-}
-
 @media screen and (max-width: 992px) {
   .blog-tiles {
     width: 90%;
     margin-right: 5%;
     margin-left: 5%;
-  }
-
-  .blog-item .details {
-    padding-left: 0px;
-    padding-right: 0px;
-  }
-}
-
-@media screen and (max-width: 800px) {
-  img {
-    width: 100% !important;
-    height: 100% !important;
   }
 }
 </style>
@@ -116,12 +76,37 @@ p {
 import Logo from "src/components/generic/logo.vue";
 import MyFooter from "src/components/frame/footer.vue";
 import ROUTER from "router";
+import Config from "src/config.js";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 export default {
+  mounted() {
+    this.getContent();
+    window.scrollTo(0, 0);
+  },
   components: {
     Logo,
     MyFooter,
   },
+  data() {
+    return {
+      content: null,
+      loading: true,
+      faCircleNotch: faCircleNotch,
+    };
+  },
   methods: {
+    async getContent() {
+      const { GoogleSpreadsheet } = require("google-spreadsheet");
+      const doc = new GoogleSpreadsheet(Config.googleSheetId);
+      await doc.useServiceAccountAuth({
+        client_email: Config.google.client_email,
+        private_key: Config.google.private_key,
+      });
+      await doc.loadInfo();
+      const blogs = doc.sheetsByIndex[14];
+      let blogsRows = await blogs.getRows();
+      this.content = blogsRows[0].content;
+    },
     redirect(params) {
       ROUTER.push(params);
     },
